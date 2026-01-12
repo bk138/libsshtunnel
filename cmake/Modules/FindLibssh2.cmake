045c94c2e508f0b48e95e6bdede23a6387a06f3e
Christian Beier <info@christianbeier.net>
2026-01-12 20:04:30 +0100

    build: adapt libssh2 CMake module for Conan

--- cmake/Modules/FindLibssh2.cmake
+++ cmake/Modules/FindLibssh2.cmake
@@ -22,22 +22,22 @@
 # - Try to find the libssh2 library
 # Once done this will define
 #
-# LIBSSH2_FOUND - system has the libssh2 library
-# LIBSSH2_INCLUDE_DIR - the libssh2 include directory
-# LIBSSH2_LIBRARY - the libssh2 library name
+# Libssh2_FOUND - system has the libssh2 library
+# Libssh2_INCLUDE_DIR - the libssh2 include directory
+# Libssh2_LIBRARY - the libssh2 library name
 
-find_path(LIBSSH2_INCLUDE_DIR libssh2.h)
+find_path(Libssh2_INCLUDE_DIR libssh2.h)
 
-find_library(LIBSSH2_LIBRARY NAMES ssh2 libssh2)
+find_library(Libssh2_LIBRARIES NAMES ssh2 libssh2)
 
-if(LIBSSH2_INCLUDE_DIR)
-  file(STRINGS "${LIBSSH2_INCLUDE_DIR}/libssh2.h" libssh2_version_str REGEX "^#define[\t ]+LIBSSH2_VERSION[\t ]+\"(.*)\"")
-  string(REGEX REPLACE "^.*\"([^\"]+)\"" "\\1"  LIBSSH2_VERSION "${libssh2_version_str}")
+if(Libssh2_INCLUDE_DIR)
+  file(STRINGS "${Libssh2_INCLUDE_DIR}/libssh2.h" libssh2_version_str REGEX "^#define[\t ]+LIBSSH2_VERSION[\t ]+\"(.*)\"")
+  string(REGEX REPLACE "^.*\"([^\"]+)\"" "\\1"  Libssh2_VERSION "${libssh2_version_str}")
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LibSSH2
-    REQUIRED_VARS LIBSSH2_LIBRARY LIBSSH2_INCLUDE_DIR
-    VERSION_VAR LIBSSH2_VERSION)
+find_package_handle_standard_args(Libssh2
+    REQUIRED_VARS Libssh2_LIBRARIES Libssh2_INCLUDE_DIR
+    VERSION_VAR Libssh2_VERSION)
 
-mark_as_advanced(LIBSSH2_INCLUDE_DIR LIBSSH2_LIBRARY)
+mark_as_advanced(Libssh2_INCLUDE_DIR Libssh2_LIBRARIES)
